2a5bf592e8078bbe7c07e0f1b515b2111969bbe4
Owen Young <theowenyoung@users.noreply.github.com>
2023-11-23 14:11:13 +0800

    update lsp

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -19,7 +19,7 @@ for _, lsp in ipairs(servers) do
   }
   if lsp == "tsserver" then
     options.root_dir = parent_lspconfig.util.root_pattern "package.json"
-    options.single_file_support = true
+    options.single_file_support = false
   end
   if lsp == "denols" then
     options.root_dir = parent_lspconfig.util.root_pattern("deno.json", "deno.jsonc")
