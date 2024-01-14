4d7779fcf1203f2877cb3c19375eb77475edf1cb
Owen Young <theowenyoung@users.noreply.github.com>
2024-01-15 06:12:37 +0800

    udpate init

--- .config/clash/init.sh
+++ .config/clash/init.sh
@@ -8,6 +8,11 @@ if [[ "$OSTYPE" != "linux-gnu"* ]]; then
 	exit 1
 fi
 
+# check is root
+if [ "$(id -u)" != "0" ]; then
+	echo "Error: You must be root to run this script!"
+	exit 1
+fi
 # add to systemd service
 
 # wget data first
@@ -16,6 +21,56 @@ wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/202401132
 # use run.sh
 
 PNAME=clash
+
+function cleanup {
+	EXIT_CODE=$?
+	set +e # disable termination on error
+	# check if /etc/resolv.conf.bak exists
+	echo cleanup dns and ipforward
+	if [ -f "/etc/resolv.conf.bak" ]; then
+		echo "file /etc/resolv.conf.bak exists, restore it"
+		sudo cp /etc/resolv.conf.bak /etc/resolv.conf
+	else
+		echo "file /etc/resolv.conf.bak not exists"
+	fi
+	sysctl -w net.ipv4.ip_forward=0
+	exit $EXIT_CODE
+}
+trap cleanup EXIT
+
+# check if bak exists
+if [ -f "/etc/resolv.conf.bak" ]; then
+	echo "file /etc/resolv.conf.bak exists, do nothing"
+else
+	echo "file /etc/resolv.conf.bak not exists, backup it"
+	sudo cp /etc/resolv.conf /etc/resolv.conf.bak
+fi
+
+# backup /etc/resolv.conf
+
+# check /tmp/clash_started is first time run this script, if so sleep 10s to wait for network to be ready
+
+if [ ! -f "/tmp/clash_started" ]; then
+	echo "file /tmp/clash_started not exists, sleep 10s to wait for network to be ready"
+	sleep 10
+fi
+# create /tmp/clash_started
+touch /tmp/clash_started
+
+# change the dns server to 127.0.0.1
+
+dnsresolv=$(
+	cat <<-END
+		nameserver 127.0.0.1
+		nameserver 119.29.29.29
+	END
+)
+echo "$dnsresolv" >/etc/resolv.conf
+
+# from https://lancellc.gitbook.io/clash/start-clash/clash-tun-mode/setup-system-stack-in-fake-ip-mode
+
+# set ip forward
+sysctl -w net.ipv4.ip_forward=1
 
 service_content="$(
 	cat <<EOF
