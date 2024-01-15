a50c8011d14e16e6c2e7385a8c196b04fcc04044
Owen Young <theowenyoung@users.noreply.github.com>
2024-01-15 08:20:58 +0800

    update up

---
+++ .config/sslocal/up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# service for user service
+# we alread has ss service on ~/.config/systemd/user/ss.service
+
+UNIT=ss
+
+systemctl --user enable $UNIT
+
+systemctl --user daemon-reload
+systemctl --user restart $UNIT
+systemctl --user status $UNIT
