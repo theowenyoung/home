9c23fcc61e7eaf63a883e5736f24590eba4c813f
Owen Young <theowenyoung@users.noreply.github.com>
2024-01-21 21:04:36 +0800

    update flake

--- envs/flake.nix
+++ envs/flake.nix
@@ -152,7 +152,7 @@
         infisical
         sops
         (pkgs.callPackage ./packages/clash-meta/default.nix {})
-        # (pkgs.callPackage ./packages/daed/default.nix {})
+        (pkgs.callPackage ./packages/daed/default.nix {})
 
       ];
     };
