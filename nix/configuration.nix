82f402697664b516ae8e77734af922db8ed1e24c
Owen Young <theowenyoung@users.noreply.github.com>
2024-05-03 02:29:40 +0800

    update

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -52,6 +52,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINAPcRy9wGjP47bHpv2RcNO3yw3udCcTlgWs22KLcpUW main@example.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTAROXsvgKDzztILO8mPPRv6HZamQa7M05kzNM8gtQZ root@nixos"
   ];
 
   system.stateVersion = "23.11";
