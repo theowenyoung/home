22f025d6bebe1b264aa679bba19132e638f62ab8
Owen Young <theowenyoung@users.noreply.github.com>
2024-05-03 02:21:44 +0800

    update search

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINAPcRy9wGjP47bHpv2RcNO3yw3udCcTlgWs22KLcpUW main@example.com";
-  systems = [ system1 ];
+  nixos1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTAROXsvgKDzztILO8mPPRv6HZamQa7M05kzNM8gtQZ root@nixos";
+  systems = [ system1 nixos1 ];
 in
 {
-  "meilisearch.age".publicKeys = [system1 ];
+  "meilisearch.age".publicKeys = [system1 nixos1];
 }
