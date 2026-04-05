5bc9d72044b8f5e492528207bbdb752c76c3b42d
user.email <62473795+theowenyoung@users.noreply.github.com>
2026-04-05 10:56:17 +0800

    update config

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -4,8 +4,8 @@ local M = {}
 local highlights = require "highlights"
 
 M.base46 = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
+  theme = "catppuccin-latte",
+  theme_toggle = { "catppuccin-latte", "catppuccin" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
