9ce149ff3b58eeea24808c0aca4b7f1559eafb02
user.email <62473795+theowenyoung@users.noreply.github.com>
2026-04-05 14:56:00 +0800

    udpate config

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -4,8 +4,8 @@ local M = {}
 local highlights = require "highlights"
 
 M.base46 = {
-  theme = "catppuccin-latte",
-  theme_toggle = { "catppuccin-latte", "catppuccin" },
+  theme = "vscode_light",
+  theme_toggle = { "vscode_light", "vscode_dark" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
