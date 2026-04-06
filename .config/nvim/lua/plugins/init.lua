0628e2d42d7504c657bfbe73eb04e4d33e18c6ed
user.email <62473795+theowenyoung@users.noreply.github.com>
2026-04-06 09:43:45 +0800

    udpate search

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -47,6 +47,25 @@ return {
   },
 
   {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",
+          "--glob=!.git/",
+        },
+      },
+    },
+  },
+
+  {
     "alexghergh/nvim-tmux-navigation",
     lazy = false,
     config = function()
